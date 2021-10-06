bb4cb73e62c263251b1dee4bd97cecc22fe380f8
unknown <filipovicmilica94@gmail.com>
2021-10-06 23:02:21 +0200

    definder added

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function search(){
     alert("serching...");
 }
+
+function switchmode(mode){
+    var btn2=document.getElementById("light");
+    var btn1=document.getElementById("dark");
+    if(mode==1){//dark
+        
+    }
+    else{//light
+        
+    }
+}
