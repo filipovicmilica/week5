a4223c4a2597a2b4f2e8aa1e8a86eb8828e21093
unknown <filipovicmilica94@gmail.com>
2021-10-07 17:54:28 +0200

    search changed

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+var btnmode=document.getElementById('btnmode');
+var body=document.querySelector('body');
+var  devfinder=document.getElementById('devfinder');
+var  srch=document.getElementById('srch');
 
-let btnmode=document.getElementById('btnmode');
-let body=document.querySelector('body');
-let  devfinder=document.getElementById('devfinder');
 btnmode.onclick=function(){
 
     body.classList.toggle('dark-mode');//ubacuje/izbacuje ovu klasu ako je nema/ima
     btnmode.classList.toggle('btn-light');
+    srch.classList.toggle('div-dark-mode');
     
     if(btnmode.classList.contains('btn-light')){
         btnmode.textContent="light";
@@ -18,6 +20,19 @@ btnmode.onclick=function(){
     
 }
 
+//search
+
+var allUsernames=['nakica','rale123','masamasnica'];
+
+var searchtext=document.getElementById('searchtext');
+var searchmsg=document.getElementById('searchmsg');
 function search(){
-    alert("serching...");
+    alert(allUsernames.length);
+    for(let i=0;i<allUsernames.length;i++){
+        if(allUsernames[i]==searchtext.value){
+            searchmsg.style.display='none';
+            return;
+        }
+    }
+    searchmsg.style.display='inline-block';
 }
