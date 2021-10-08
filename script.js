095d394b6faae03461379100904dd18a8fe0cbf1
unknown <filipovicmilica94@gmail.com>
2021-10-08 14:42:24 +0200

    center added

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 var btnmode=document.getElementById('btnmode');
 var body=document.querySelector('body');
-var  devfinder=document.getElementById('devfinder');
-var  srch=document.getElementById('srch');
-var  info=document.getElementById('info');
+var devfinder=document.getElementById('devfinder');
+var srch=document.getElementById('srch');
+var info=document.getElementById('info');
+var h1=document.getElementById('h1');
+var footerElems=document.getElementsByClassName('elem');
 
 btnmode.onclick=function(){
 
     body.classList.toggle('dark-mode');//ubacuje/izbacuje ovu klasu ako je nema/ima
     btnmode.classList.toggle('btn-light');
+    btnmode.classList.toggle('btn-dark');
+
     srch.classList.toggle('div-dark-mode');
     info.classList.toggle('div-dark-mode');
+    for(let i=0;i<footerElems.length;i++){
+        footerElems[i].classList.toggle('white-mode');
+    }
 
     if(btnmode.classList.contains('btn-light')){
         btnmode.textContent="light";
         devfinder.style.color="#FFFFFF";
+        h1.style.color="#FFFFFF";
     }
     else{
         btnmode.textContent="dark";
         devfinder.style.color="#222731";
+        h1.style.color="#2B3442";
     }
     
 }
